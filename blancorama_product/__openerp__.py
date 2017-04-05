8f891866d44bf64e9891181733f26f54c833a918
dOn MiKe <donmikebautista@gmail.com>
2017-04-05 06:38:27 +0000

    TraducciOn y Conekta

--- blancorama_product/__openerp__.py
+++ blancorama_product/__openerp__.py
@@ -20,13 +20,18 @@
     'version': '0.1',
 
     # any module necessary for this one to work correctly
-    'depends': ['base','product'],
+    'depends': [
+        'base',
+        'product',
+        'payment',
+        'website_sale'
+    ],
 
     # always loaded
     'data': [
         # 'security/ir.model.access.csv',
         # 'views/views.xml',
-        # 'views/templates.xml',
+        'views/templates.xml',
     ],
     # only loaded in demonstration mode
     'demo': [
